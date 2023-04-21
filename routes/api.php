781da0231592557d416425b7bc0d7f7dad7300db
AhmedWael97 <ahmed.wael010166@gmail.com>
2023-04-21 19:06:04 +0200

    fixMethodInAPI

--- routes/api.php
+++ routes/api.php
@@ -47,7 +47,7 @@ Route::prefix('v1')->group(function () {
 
     Route::prefix('order')->group(function() {
         Route::controller(OrderController::class)->group(function() {
-            Route::get('/store','store')->middleware('auth:sanctum');
+            Route::post('/store','store')->middleware('auth:sanctum');
 
         });
     });
