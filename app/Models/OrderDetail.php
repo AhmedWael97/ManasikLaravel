35dba06ef20bc0094e660eead227e5d0a589d86f
AhmedWael97 <ahmed.wael010166@gmail.com>
2023-05-21 00:25:45 +0300

    fixErrorOnOrderDetail

--- app/Models/OrderDetail.php
+++ app/Models/OrderDetail.php
@@ -10,6 +10,9 @@ class OrderDetail extends Model
     use HasFactory;
     protected $fillable = ['order_id','service_id','order_status_id','full_name','no_of_kfara','price','currency_id','price','executer_price','no_of_kfara','kfarat_choice_id','purpose_hag_id'];
 
+    public function status () {
+        return $this->hasOne('\App\Models\Status','id','order_status_id');
+    }
     public function service() {
         return $this->hasOne('\App\Models\Service','id','service_id')->select('id','name_en','name_ar','photo','max_limit','price');
     }
